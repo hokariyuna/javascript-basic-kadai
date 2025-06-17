a2b8af6dcce423c0522baf22301003beab38b320
sekineyuuna <sekine.yuna0@gmail.com>
2025-06-17 18:11:49 +0900

    課題:関数をMDNで調べて使ってみよう

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,9 @@
+const date = new Date;
+
+const year = date.getFullYear();
+
+const month = date.getMonth();
+
+const todayDate = date.getDate();
+
+console.log(year + '年' + month + '月' + todayDate + '日');
